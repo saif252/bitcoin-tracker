f156eb64bb9bf47a289e4db70eab3ed1551ee8c2
Shawkatul Islam Sarker Saif <56231332+saif252@users.noreply.github.com>
2025-12-14 15:04:31 +0800

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -6,13 +6,12 @@ DB_PASS=""
 DB_NAME="crypto_db"
 ASSET_ID=1
 
-
 # Price Last 24 Hour
 price_24hr() {
     TXT_FILE="price_24hr.txt"
     OUTPUT_PNG="price_24hr.png"
-    # Save the relevent timestamp and prices to price_24hr.txt
-    /Applications/XAMPP/xamppfiles/bin/mysql -u "$DB_USER" -h 127.0.0.1 -P 3306 -D "$DB_NAME" -B -N -e "
+    # save Relevent timestamp and prices to price_24hr.txt
+    mysql -u "$DB_USER" -h 127.0.0.1 -P 3306 -D "$DB_NAME" -B -N -e "
         SELECT DATE_FORMAT(timestamp,'%Y-%m-%d %H:%i:%s'), price
         FROM asset_metrics
         WHERE asset_id=$ASSET_ID
@@ -20,13 +19,13 @@ price_24hr() {
         ORDER BY timestamp;
     " > "$TXT_FILE"
 
-    # Check if file has content
+    # Check File not empty
     if [[ ! -s "$TXT_FILE" ]]; then
         echo "Error: '$TXT_FILE' is empty. Cannot plot."
         exit 1
     fi
 
-    # Plot (Gnuplot)
+    # Gnuplot
     gnuplot <<-EOF
         set terminal png size 1000,600
         set output "$OUTPUT_PNG"
@@ -44,15 +43,41 @@ EOF
     echo "Price (24hr) plot saved to $OUTPUT_PNG"
 }
 
-
 # Main Menu
 case "$1" in
     price_24hr)
         price_24hr
         ;;
+    price_7days)
+        price_7days
+        ;;
+    percent-change_24hr)
+        percent_change_24hr
+        ;;
+    mcap_fdv)
+        mcap_fdv
+        ;;
+    volume_24hr)
+        volume_24hr
+        ;;
+    circulating_supply)
+        circulating_supply_7days
+        ;;
+    market_cap)
+        market_cap_7days
+        ;;
+    vol_mcap)
+        vol_mcap_24hr
+        ;;
+    price_volume-mcap)
+        price_volume-mcap_24hr
+        ;;
+    mcap_price)
+        mcap_price
+    ;;
     *)
         echo "Unknown plot type: $1"
-        echo "Available types: price_24hr"
+        echo "Available types: price_24hr, fdv_7days, percent-change_24hr, mcap-fdv, voume_24hr, circulating_supply, market_cap, vol_mcap, price_volume-mcap, mcap_price"
         exit 1
         ;;
 esac
